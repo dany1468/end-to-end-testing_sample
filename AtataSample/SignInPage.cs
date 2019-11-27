8cf0550e2b524cf286f3362a2849ba70aad59667
dany1468 <dany1468@gmail.com>
2019-11-28 06:25:30 +0900

    Add Atata basic Sample

---
+++ AtataSample/SignInPage.cs
@@ -0,0 +1,18 @@
+﻿using Atata;
+
+namespace AtataSample
+{
+    using _ = SignInPage;
+
+    [Url("signin")]
+    [VerifyTitle]
+    [VerifyH1]
+    public class SignInPage : Page<_>
+    {
+        public TextInput<_> Email { get; private set; }
+
+        public PasswordInput<_> Password { get; private set; }
+
+        public Button<_> SignIn { get; private set; }
+    }
+}
---
+++ AtataSample/SignInTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Atata;
+using NUnit.Framework;
+namespace AtataSample
+{
+    [TestFixture]
+    public class SignInTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            AtataContext.Configure().
+                UseChrome().
+                UseBaseUrl("https://demo.atata.io/").
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                AddScreenshotFileSaving().
+                LogNUnitError().
+                TakeScreenshotOnNUnitError().
+                Build();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            AtataContext.Current?.CleanUp();
+        }
+
+        [Test]
+        public void SignIn()
+        {
+            Go.To<SignInPage>().
+                Email.Set("admin@mail.com").
+                Password.Set("abc123").
+                SignIn.Click();
+        }
+    }
+}

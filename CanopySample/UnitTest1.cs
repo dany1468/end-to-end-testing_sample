1b5c6156dd944810b71afd89285de50ba6ae9dec
dany1468 <dany1468@gmail.com>
2019-11-28 10:24:50 +0900

    Add canopy basic Sample

--- CanopySample/UnitTest1.cs
+++ CanopySample/UnitTest1.cs
@@ -1,5 +1,7 @@
 using System;
 using Xunit;
+using canopy;
+using _ = canopy.csharp.canopy;
 
 namespace CanopySample
 {
@@ -8,7 +10,19 @@ namespace CanopySample
         [Fact]
         public void Test1()
         {
+            var testpage = "http://lefthandedgoat.github.io/canopy/testpages/";
 
+            _.start(types.BrowserStartMode.Chrome);
+
+            _.test("Apostrophes don't break anything", () =>
+            {
+                _.url(testpage);
+                _.count("I've got an apostrophe", 1);
+            });
+
+            _.run();
+
+            _.quit();
         }
     }
 }
